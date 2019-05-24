0680c7175a3a5c60028c640890ff09ebb8891172
konuma1977 <koichi.numajiri@gmail.com>
2019-05-24 19:24:31 +0900

    Add GenPrivateKey

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # OpenSSL-Tutorial
-## OpenSSL Sample Code  
+## OpenSSL Sample Code
+English text is below.  
 * OpenSSLを使ったハッシュ値の計算 (/src/Hash)  
 	* ハッシュ関数SHA-256とは  
 	* ハッシュ関数と共に利用されるSaltとは  
+* OpenSSLを使った暗号鍵の作成 (/src/GenPrivateKey)  
+	* パスワードから暗号鍵を作る  
+	* 暗号鍵を自動的に作成する  
+* OpenSSLを使った鍵ペアの生成 (/src/GenKeypair) ※作成中  
+* 暗号鍵を安全に保存する手段 (/src/pkcs12) ※作成中  
+* OpenSSLを使った暗号文の生成 (/src/Encrypt) ※作成中  
+* OpenSSLを使った電子署名 (/src/Sign) ※作成中  
+
+* Hash Calculation by OpenSSL (/src/Hash)  
+	* What is SHA-256 ?  
+	* What is salt to be used with hash
+* Private Key generation by OpenSSL (/src/GenPrivateKey)  
+	* Derives private key from password
+	* Generate private key from random
+* Generate key pair by OpenSSL(/src/GenKeypair) ... not yet      
+* Secure way to store private key (/src/pkcs12) ... not yet  
+* Encrypt plain text by OpenSSLを(/src/Encrypt) ... not yet  
+* Sign document by OpenSSL (/src/Sign) ... not yet  
   
 解説 https://numajiri.blog/
---
+++ src/GenPrivateKey/GenPrivateKey.cpp
@@ -0,0 +1,61 @@
+﻿// GenPrivateKey.cpp : パスワードを使った暗号化 (Password Based Encryption)
+//           PKCS#5-PBKDF2, SHA256
+
+#include <cstdio>
+#include <openssl/pem.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+int main()
+{
+	OpenSSL_add_all_algorithms();
+
+	// ---------------------------------------------------------------------------
+	// Derives private key from password (PKCS#5)
+	// ---------------------------------------------------------------------------
+	const char* pass = "password";
+	int iterations = 10000; // as of 2019, should be higher than 10,000
+	unsigned char salt[16]; // random for protecting from directional attack
+	RAND_bytes(salt, 16);
+
+	unsigned char privatekey[16]; // derived private key, 16byte=128bit
+
+	if (PKCS5_PBKDF2_HMAC(pass, strlen(pass), salt, sizeof(salt) / sizeof(unsigned char), iterations, EVP_sha256(), 16, privatekey) == 1) // RFC2898
+	{
+		char hexout[16 * 2 + 1];
+		for (unsigned int i = 0; i < 16; ++i)
+		{
+			sprintf_s(&hexout[i * 2], 3, "%02x", 0xFF & privatekey[i]);
+		}
+		hexout[16 * 2] = '\0';
+		printf(hexout);
+
+		OPENSSL_cleanse(hexout, sizeof(privatekey));
+		OPENSSL_cleanse(hexout, sizeof(hexout));
+	}
+
+	// ---------------------------------------------------------------------------
+	// Generate private key automatically
+	// ---------------------------------------------------------------------------
+	RAND_bytes(privatekey, 16);
+
+	// ---------------------------------------------------------------------------
+	// Secure way to store private key (PKCS#8)
+	// ---------------------------------------------------------------------------
+
+	/*
+	if (EVP_CIPHER_CTX_init(&c))
+	{
+		EVP_EncryptInit(&c, EVP_aes_128_ecb(), password, nullptr);
+		EVP_EncryptUpdate(&context, encrypt.data(), &len, message,
+			static_cast<int>(str.length() + EVP_MAX_BLOCK_LENGTH));
+		EVP_EncryptFinal(&context, encrypt.data() + len, &len);
+
+		EVP_CIPHER_CTX_cleanup(&c); // erase EVP_CIPHER_CTX
+	}
+	*/
+
+
+
+	return 0;
+}
